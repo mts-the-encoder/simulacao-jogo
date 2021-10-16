430e15bb49abe0a20984260497a97acfcf0a5166
zmtheus <matheus.nsantos@bandtec.com.br>
2021-10-16 03:00:22 -0300

    validações e correções

--- game.js
+++ game.js
@@ -11,8 +11,29 @@ function inciar_game() {
         alert("VOCÊ DEVE ESCOLHER DOIS LUTADORES PARA CONTINUAR")
     } else if (lutador2 == 'selecionar_p2') {
         alert("VOCÊ DEVE ESCOLHER DOIS LUTADORES PARA CONTINUAR")
-    }
-    else {
+    } else if (tela == 'tela-ryu') {
+        fundo.style.display = 'block';
+    } else if (tela = 'tela-ken') {
+        fundo.style.display = 'block';
+    } else if (tela = 'tela-guile') {
+        fundo.style.display = 'block';
+    } else if (tela = 'tela-chun') {
+        fundo.style.display = 'block';
+    } else if (tela = 'tela-zangief') {
+        fundo.style.display = 'block';
+    } else if (tela = 'tela-honda') {
+        fundo.style.display = 'block';
+    } else if (tela = 'tela-dhalsim') {
+        fundo.style.display = 'block';
+    } else if (tela = 'tela-balrog') {
+        fundo.style.display = 'block';
+    } else if (tela = 'tela-vega') {
+        fundo.style.display = 'block';
+    } else if (tela = 'tela-bison') {
+        fundo.style.display = 'block';
+    } else if (tela == 'tela-blanka') {
+        fundo.style.display = 'block'
+    } else if (tela == 'tela=sagat') {
         fundo.style.display = 'block'
     }
 }
